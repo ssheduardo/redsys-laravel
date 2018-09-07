16e808844251c461cb8ab0c92845dd1d697ee93b
Eduardo D <ssh.eduardo@gmail.com>
2018-09-07 15:31:38 +0200

    Changed config.php, uses env variables.

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,11 @@
 <?php
 return [
-    'key'                   => '',
-    'url_notification'      => '',
-    'url_ok'                => '',
-    'url_ko'                => '',
+    'key'                   => env('REDSYS_KEY',''),
+    'url_notification'      => env('REDSYS_URL_NOTIFICATION',''),
+    'url_ok'                => env('REDSYS_URL_OK',''),
+    'url_ko'                => env('REDSYS_URL_KO',''),
+    'merchantcode'          => env('REDSYS_MERCHANT_CODE',''),
+    'terminal'              => env('REDSYS_TERMINAL','1'),
+    'enviroment'            => env('REDSYS_ENVIROMENT','test'),
+    'tradename'             => env('REDSYS_TRADENAME',''),
 ];
